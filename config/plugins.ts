483b793ec315d8184340ffe34ccd57ac77deb64d
KallistaKomdeur <k.j.komdeur@student.tue.nl>
2026-07-07 15:15:05 +0200

    Updated backend for additional registration fields.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,24 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: [
+          "firstName",
+          "lastName",
+          "phoneNumber",
+          "dateOfBirth",
+          "studyInstitutionEnum",
+          "studyInstitutionOther",
+          "graduationYear",
+          "motivationNotStudent",
+        ],
+      },
+    },
+  },
+});
 
 export default config;
